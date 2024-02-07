2cf6b730c9cf4fc6b8b64218df41435896f4836a
Pankaj Tijare <127303900+PankajTijare@users.noreply.github.com>
2024-02-07 13:01:16 +0530

    Update app_health_checker.sh

--- app_health_checker.sh
+++ app_health_checker.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-APP_URL="https://www.inbestments.com/app/"  # Replace with your application URL
-
+APP_URL="https://www.inbestments.com/app/" 
 check_application_status() {
-    # Make an HTTP request to the application
     response_code=$(curl -s -o /dev/null -w "%{http_code}" "$APP_URL")
 
-    # Check the HTTP status code
     if [ "$response_code" == "200" ]; then
         echo "Application is UP. HTTP Status Code: $response_code"
         exit 0
@@ -16,5 +12,4 @@ check_application_status() {
     fi
 }
 
-# Run the application status check function
 check_application_status
